f92f63b63575e1d92e16e36f75f98289a7d3d736
k2y <marycarychin@gmail.com>
2022-08-30 14:51:19 +0900

    feat: ongoing err

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import {
   Categories,
   categoryState,
@@ -32,18 +32,62 @@ const ToDoBtn = styled.button`
     box-shadow: ${(prop) => prop.theme.clickedStyle};
   }
 `;
+const CategoryContainer = styled.div`
+  border: 1px green;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  margin-top: 5px;
+  margin-bottom: 15px;
+  select {
+    width: 70%;
+    background-color: ${(prop) => prop.theme.whiteColor};
+    border: none;
+    box-shadow: ${(prop) => prop.theme.inputStyle};
+    border-radius: 10px;
+    padding: 10px;
+    margin: 5px 0px 10px 0px;
+  }
+`;
 
 function ToDoList() {
   const [toDo, setToDo] = useRecoilState(toDoState);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const toDos = useRecoilValue(toDoSelector);
+  const [customCategories, setCustomCategories] = useRecoilState(
+    customCategoriesState
+  );
+
+  const onInput = (event: React.FocusEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
+  };
 
   const onDelelteAllClick = (e: React.FormEvent<HTMLButtonElement>) => {
     setToDo([]);
     setCustomCategories([]);
   };
+
+  useEffect(() => {
+    localStorage.setItem('toDos', JSON.stringify(toDo));
+  }, [toDo]);
+
+  useEffect(() => {
+    localStorage.setItem('CATEGORIES_KEY', JSON.stringify(customCategories));
+  }, [customCategories]);
+
+  console.log(category);
+
   return (
     <ToDoContainer>
       <Header>
         <h1> ToDOs</h1>
+        <h2> Choose category</h2>
+        <CategoryContainer>
+          <select value={category} onInput={onInput}>
+            <option></option>
+          </select>
+        </CategoryContainer>
         <ToDoBtn onClick={onDelelteAllClick}>Reset</ToDoBtn>
       </Header>
     </ToDoContainer>
